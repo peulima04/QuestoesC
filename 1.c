c0016f9303b32d6ff6124eb3611e4cda1624ace8
Pedro Lima <99203431+peulima04@users.noreply.github.com>
2023-10-13 22:41:40 -0300

    1.c

--- 1.c
+++ 1.c
@@ -3,13 +3,10 @@
 int main() {
     float nota;
 
-    // Solicita a nota ao usuário
     printf("Digite a nota do aluno: ");
     scanf("%f", &nota);
 
-    // Verifica se a nota está dentro do intervalo válido
     if (nota >= 0 && nota <= 100) {
-        // Verifica se a nota é maior ou igual a 60
         if (nota >= 60) {
             printf("APROVADO\n");
         } else {
